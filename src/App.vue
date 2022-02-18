6e5f7426b041c9613a0b70478e8d22965210fedf
Maciej Machniewski <machniewski.maciej@gmail.com>
2022-02-18 15:50:34 +0100

    WiP

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MachineLearning msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MachineLearning from './components/MachineLearning.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MachineLearning
   }
 }
 </script>
---
+++ src/components/MachineLearning.vue
@@ -0,0 +1,72 @@
+<template>
+  <div> 
+    <h1>{{title}}</h1>
+    {{text}}
+  </div>
+</template>
+
+<script>
+import * as tf from "@tensorflow/tfjs"
+import * as tfvis from "@tensorflow/tfjs-vis"
+import { MnistData } from '../data.js';
+
+export default {
+  name: 'MachineLearning',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    title: 'tytuł',
+    text: 'some text'
+  }),
+  methods: {
+    init() {
+      tf;
+      tfvis;
+      MnistData;
+
+    }
+  },
+  created() {
+    //create a container in the visor
+    const surface = tfvis
+    .visor()
+    .surface({ name: "Imput Data Examples", tab: "Input Data" });
+    // Get the examples
+    const examples = data.nextTestBatch(20);
+    const numExamples = examples.xs.shape[0];
+    //Create a canvas element to redner each image
+    for(let i=0; i<numExamples; i++) {
+      const imageTensor = tf.tidy(() => {
+            //Reshape the image to 28 x 28 px
+            return examples.xs
+            .slice([i,0], [examples.xs.shape[1]])
+            .reshape([28, 28, 1]);
+      });
+      const canvas = document.createElement("canvas");
+      canvas.width = 28;
+      canvas.height = 28;
+      canvas.style = "margin: 4px;";
+      await tf.browser.toPixels(imageTensor, canvas);
+      surface.drawArea.appendChild(canvas);
+
+      imageTensor.dispose();
+    }
+  }
+
+ //Run the process
+    run();
+    async function run() {
+      const data = new MnistData();
+      await data.load();
+      await showExamples(data);
+    }
+  }
+};
+
+   
+</script>
+
+<style>
+
+</style>
